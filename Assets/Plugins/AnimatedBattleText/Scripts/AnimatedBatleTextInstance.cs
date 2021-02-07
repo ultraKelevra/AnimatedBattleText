e9cf332f34e554dae0bf71485a307f315c79b954
Pedro Fuentes <pedrofuenteszeg0@gmail.com>
2021-02-07 04:32:28 -0500

    Initial commit

---
+++ Assets/Plugins/AnimatedBattleText/Scripts/AnimatedBatleTextInstance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using TMPro;
+using Unity.Mathematics;
+
+
+namespace AnimatedBattleText
+{
+	[Serializable]
+    public class AnimatedTextInstace
+    {
+        public Transform[] letterTransforms;
+        public TMP_Text[][] letters;
+        public TextAnimation props;
+        public float startTime;
+        public float2 pos;
+        public bool animationFinished;
+        public bool active;
+    }
+}
---
+++ Assets/Plugins/AnimatedBattleText/Scripts/TextAnimation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+using TMPro;
+
+
+namespace AnimatedBattleText
+{
+	[CreateAssetMenu(fileName = "newTextAnimation", menuName = "BattleText/TextAnimation")]
+	public class TextAnimation: ScriptableObject
+	{
+		public enum TextAnimationAlignment
+		{
+			Left,
+			Center,
+			Right
+		}
+
+		[Header("Font Controls")]
+		public TMP_FontAsset font;
+		public float textSize = 16;
+		public TextAnimationAlignment alignment;
+
+		[Header("Time Controls")]
+		public float transitionInDuration = .5f;
+		public float perLetterDelay = 0.05f;
+		public bool invertDelay = false;
+		
+		[Header("Spacing Animation")]
+		public float initialLetterSpacing = (1 / 32.0f) * 8;
+		public float finalLetterSpacing = (1 / 32.0f) * 8;
+		public AnimationCurve pivotCurve = AnimationCurve.Constant(0, 1, 1);
+
+		[Header("Displace Animation")]
+		public float2 initialLetterAdditivePos = Vector2.zero;
+		public float2 finalLetterAdditivePos = Vector2.zero;
+		public AnimationCurve additivePosCurve = AnimationCurve.Constant(0, 1, 1);
+
+		[Header("Color Animation")]
+		public Gradient fillColorInTime;
+		public Gradient borderColorInTime;
+	}
+}
